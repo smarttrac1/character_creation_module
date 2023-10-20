b64d46b92d546202eb9e2d101811d119733ab2b9
Илья Фролов <smarttrac1@mail.ru>
2023-10-20 14:40:14 +0300

    add working version without classes

--- main.py
+++ main.py
@@ -4,6 +4,15 @@ from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Атака персонажа
+
+    Args:
+        char_name (str): Имя игрока
+        char_class (str): Название персонажа
+
+    Returns:
+        str: результат атаки персонажа
+    """
     if char_class == 'warrior':
         return (f'{char_name} нанёс противнику урон, равный '
                 f'{5 + randint(3, 5)}')
@@ -17,6 +26,15 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Защита персонажа
+
+    Args:
+        char_name (str): Имя игрока
+        char_class (str): Название персонажа
+
+    Returns:
+        str: Результат защиты персонажа
+    """
     if char_class == 'warrior':
         return (f'{char_name} блокировал {10 + randint(5, 10)} ед. урона')
     if char_class == 'mage':
@@ -27,6 +45,15 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Применение умений персонажем
+
+    Args:
+        char_name (str): Имя игрока
+        char_class (str): Название персонажа
+
+    Returns:
+        str: результат умения персонажа
+    """
     if char_class == 'warrior':
         return (f'{char_name} применил специальное умение '
                 f'«Выносливость {80 + 25}»')
@@ -38,6 +65,15 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Функция тренировки
+
+    Args:
+        char_name (str): Имя игрока
+        char_class (str): Название персонажа
+
+    Returns:
+        str: тренеровка окончена
+    """
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — великий мастер ближнего боя.')
     if char_class == 'mage':
@@ -62,6 +98,11 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Определяем класс персонажа
+
+    Returns:
+        str: Название персонажа
+    """
     approve_choice: str = ''
     char_class: str = ''
     while approve_choice != 'y':
